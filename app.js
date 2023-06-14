640a53d0b887030ea44a30fc930e265bae3b21d0
Tuanmaic <tuanmaic@nashtechglobal.com>
2023-06-14 11:14:58 +0700

    fix db config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
+const { connectToDatabase, disconnectFromDatabase } = require('./config/db');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,46 +12,46 @@ const usersRouter = require('./routes/users');
 const app = express();
 dotenv.config();
 
-main().catch(err => console.log(err));
+// Connect to the database
+connectToDatabase();
 
-async function main() {
-  // db connection
-  const dbConnection = process.env.DB_CONNECTION;
-  await mongoose.connect(dbConnection);
-  console.info('Database connected');
+// Start the server
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-  // Start the server
-  const port = process.env.PORT;
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // Middleware
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-  app.use('/', indexRouter);
-  app.use('/users', usersRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    next(createError(404));
-  });
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.send('Internal Server Error');
+});
 
-    res.status(err.status || 500);
-    res.send('Internal Server Error');
-  });
-}
+// Gracefully disconnect from the database when the application is terminated
+process.on('SIGINT', async () => {
+  await disconnectFromDatabase();
+  process.exit(0);
+});
 
 module.exports = app;
